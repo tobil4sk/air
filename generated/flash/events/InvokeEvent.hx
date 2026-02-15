0830c7407b9156672d3cc0fb80a0af6815c0a889
Tobiasz Laskowski <tobil4sk@outlook.com>
2026-02-15 01:01:27 +0000

    Fix type for get_arguments as well

--- generated/flash/events/InvokeEvent.hx
+++ generated/flash/events/InvokeEvent.hx
@@ -5,7 +5,7 @@ extern class InvokeEvent extends Event {
 	@:flash.property var currentDirectory(get,never) : flash.filesystem.File;
 	@:flash.property var reason(get,never) : String;
 	function new(type : String, bubbles : Bool = false, cancelable : Bool = false, ?dir : flash.filesystem.File, ?argv : Array<Dynamic>, reason : Null<String> = "standard") : Void;
-	private function get_arguments() : Array<Dynamic>;
+	private function get_arguments() : Array<String>;
 	private function get_currentDirectory() : flash.filesystem.File;
 	private function get_reason() : String;
 	static final INVOKE : String;
